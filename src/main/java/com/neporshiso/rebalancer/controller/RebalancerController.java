69479fb6494d6d547f5a5f9db1957d12064b95fc
Nep Orshiso <nep.orshiso@gmail.com>
2022-11-07 20:26:32 -0500

    make it easier to test new deployment

--- src/main/java/com/neporshiso/rebalancer/controller/RebalancerController.java
+++ src/main/java/com/neporshiso/rebalancer/controller/RebalancerController.java
@@ -5,6 +5,7 @@ import com.neporshiso.rebalancer.entity.RebalancedPortfolio;
 import com.neporshiso.rebalancer.service.RebalancerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,11 @@ public class RebalancerController {
 
     @Autowired
     RebalancerService rebalancerService;
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello";
+    }
 
     @PostMapping("/portfolio")
     public RebalancedPortfolio rebalancePortfolio(@RequestBody Portfolio p) {
